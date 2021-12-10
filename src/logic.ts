bdcdaaecae5305e5f664ebce8bef7e1fb9cfaebe
Stavros <snikoletopoulos@hotmail.com>
2021-12-10 08:52:09 +0200

    Use arrow functions

--- src/logic.ts
+++ src/logic.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { ICountry } from "./types";
 
-export async function fetchCountries() {
+export const fetchCountries = async () => {
   const response = await axios.get("https://restcountries.com/v3.1/all");
   if (response.status !== 200) {
     throw new Error(response.status.toString());
@@ -10,8 +10,8 @@ export async function fetchCountries() {
   return countries.filter((country: ICountry) => country.borders.length > 0);
 }
 
-export function pickRandomCountryWithBorders(noBorderCountries, history) {
-  const mainCountry = shuffleArray(noBorderCountries)[0];
+export const pickRandomCountryWithBorders = (noBorderCountries: ICountry[], history): ICountry => {
+  const mainCountry = shuffleArray<ICountry>(noBorderCountries)[0];
   if (!history.includes(mainCountry)) {
     history.push(mainCountry);
     return mainCountry;
@@ -20,7 +20,7 @@ export function pickRandomCountryWithBorders(noBorderCountries, history) {
   }
 }
 
-export function cardPick(mainCountry: ICountry) {
+export const cardPick = (mainCountry: ICountry) => {
   const neighbours = [
     ...countries.filter(country => mainCountry.borders.includes(country.code3)),
   ];
@@ -35,9 +35,6 @@ export function cardPick(mainCountry: ICountry) {
   return shuffleArray(neighbours);
 }
 
-// shuffle array https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
-// επιστρέφει τον πίνακα array με τα ίδια στοιχεία σε τυχαίες θέσεις
-// χρειάζεται π.χ. για την επιλογή μιας τυχαίας χώρας ή τυχαίων γειτόνων
 const shuffleArray = <T>(array: Array<T>): Array<T> => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i);
