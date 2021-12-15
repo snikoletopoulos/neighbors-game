a672c9fccdf019234fb77a30e10f2e024be9501c
Stavros <snikoletopoulos@hotmail.com>
2021-12-15 08:44:23 +0200

    Use key name from API object

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ import type { ILoadingCountry } from "./App";
 
 const Title = ({ country }: Props) => (
   <section id="my-country">
-    <span id="my-country-flag">{getEmojiForCountry(country.alpha2Code)}</span>
+    <span id="my-country-flag">{getEmojiForCountry(country.cca2)}</span>
     <h1 id="my-country-name">{country.name}</h1>
   </section>
 );
