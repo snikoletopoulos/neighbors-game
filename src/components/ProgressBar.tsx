fd6ce10c17d535fdf5465244e5cc0233e9151e05
Stavros <snikoletopoulos@hotmail.com>
2022-01-07 03:17:24 +0200

    Switch to using Context store

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,13 +1,29 @@
-import styles from './ProgressBar.module.scss';
+import { useState, useEffect, useContext } from "react";
+import styles from "./ProgressBar.module.scss";
+import GameInfoContext from "../store/game-info-context";
 
-const ProgressBar = ({ progress }: Props) => (
-  <section id="progress" className={styles['progress-bar']}>
-    <div id='current-progress' style={{ width: progress + "%" }} className={styles['progress-bar__persentage']}></div>
-  </section>
-);
+const ProgressBar = () => {
+  const gameState = useContext(GameInfoContext);
+  const [progress, setProgress] = useState(0);
+
+  const totalCorrectAnswers = gameState.mainCountry?.borders?.length ?? 0;
+
+  useEffect(() => {
+    const progress =
+      (100 * gameState.roundInfo.rightAnswers) / totalCorrectAnswers;
+    // console.log(progress);
+    setProgress(progress);
+  }, [gameState.mainCountry, gameState.roundInfo.rightAnswers]);
+
+  return (
+    <section id="progress" className={styles["progress-bar"]}>
+      <div
+        id="current-progress"
+        style={{ width: progress + "%" }}
+        className={styles["progress-bar__persentage"]}
+      ></div>
+    </section>
+  );
+};
 
 export default ProgressBar;
-
-interface Props {
-  progress: number;
-}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,40 +1,41 @@
+import { useContext } from "react";
 import styles from "./Sidebar.module.scss";
+import GameInfoContext from "../store/game-info-context";
+
 import Button from "./UI/Button";
 
-const Sidebar = ({ round, score, gameState, reset, next }: Props) => (
-  <aside className={styles["sidebar"]} id="sidebar">
-    <h1>
-      Βρες τους
-      <br />
-      γείτονες
-    </h1>
-    <div className="sidebar__info-container">
-      <p className="sidebar__info-container__label">Γύρος:</p>
-      <p className="sidebar__info-container__value">{round}</p>
-      <p className="sidebar__info-container__label">Σκορ:</p>
-      <p className="sidebar__info-container__value" id="score">
-        {score}
-      </p>
-    </div>
-    <Button
-      className={styles["sidebar__btn"]}
-      active={gameState}
-      onClick={next}
-    >
-      Επόμενη χώρα
-    </Button>
-    <Button className={styles["sidebar__btn"]} onClick={reset}>
-      Νέο παιχνίδι
-    </Button>
-  </aside>
-);
+const Sidebar = () => {
+  const gameInfo = useContext(GameInfoContext);
+
+  const { roundInfo, resetGame, nextRound } = gameInfo;
+
+  return (
+    <aside className={styles["sidebar"]} id="sidebar">
+      <h1>
+        Βρες τους
+        <br />
+        γείτονες
+      </h1>
+      <div className="sidebar__info-container">
+        <p className="sidebar__info-container__label">Γύρος:</p>
+        <p className="sidebar__info-container__value">{roundInfo.round}</p>
+        <p className="sidebar__info-container__label">Σκορ:</p>
+        <p className="sidebar__info-container__value" id="score">
+          {roundInfo.score}
+        </p>
+      </div>
+      <Button
+        className={styles["sidebar__btn"]}
+        active={roundInfo.hasGameEnded}
+        onClick={nextRound}
+      >
+        Επόμενη χώρα
+      </Button>
+      <Button className={styles["sidebar__btn"]} onClick={resetGame}>
+        Νέο παιχνίδι
+      </Button>
+    </aside>
+  );
+};
 
 export default Sidebar;
-
-interface Props {
-  round: number;
-  score: number;
-  gameState: boolean;
-  reset: () => void;
-  next: () => void;
-}
