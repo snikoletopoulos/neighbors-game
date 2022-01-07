0b97adc944e741ee1dce6b167e68faea0c045c4b
Stavros <snikoletopoulos@hotmail.com>
2022-01-07 03:14:26 +0200

    Move logic to Context store

--- src/App.tsx
+++ src/App.tsx
@@ -1,145 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import GameInfoContext from "./store/game-info-context";
 import "./global.scss";
 
 import Sidebar from "./components/Sidebar";
 import Title from "./components/Title";
 import ProgressBar from "./components/ProgressBar";
 import CardArea from "./components/Neighbors/CardArea";
-import Card from "./components/Neighbors/Card";
+import CountryCard from "./components/Neighbors/CountryCard";
 
-import {
-  fetchCountries,
-  pickRandomCountryWithBorders,
-  cardPick,
-  filterCountries
-} from "./logic";
-
-import type ICountry from "./components/types/country.interface";
-
-let mainCountryHistory: ICountry[] = [];
-let noBorderCountries: ICountry[] = [];
-let countries: ICountry[] = [];
+import { cardPick } from "./logic";
+import ICountry from "./types/country.interface.js";
 
 const App = () => {
-  const [progress, setProgress] = useState(0);
+  const gameState = useContext(GameInfoContext);
   const [countryCards, setCountryCards] = useState<ICountry[]>([]);
-  const [gameState, setGameState] = useState<IGameState>({
-    round: 1,
-    score: 0,
-    mainCountry: {
-      name: { official: "Loading", common: "Loading" },
-      cca2: "",
-      borders: [],
-    },
-    hasGameEnded: false,
-    rightAnswers: 0,
-  });
-
-  //Fetching all the countries data
-  useEffect(() => {
-    (async () => {
-      countries = await fetchCountries();
-      noBorderCountries = filterCountries(countries);
-      setGameState(
-        (prevValue: IGameState): IGameState => ({
-          ...prevValue,
-          mainCountry: pickRandomCountryWithBorders(
-            noBorderCountries,
-            mainCountryHistory
-          ),
-        })
-      );
-    })();
-  }, []);
 
   // Updating the cards when the title changes
   useEffect(() => {
-    setCountryCards(cardPick(gameState.mainCountry as ICountry, countries));
-  }, [gameState.mainCountry]);
-
-  // Reseting the game
-  const gameReset = () => {
-    if (!confirm("Σίγουρα; Θα χάσετε όλο σας το σκορ!")) return;
-    setProgress(0);
-    setGameState(prevValue => ({
-      ...prevValue,
-      mainCountry: pickRandomCountryWithBorders(
-        noBorderCountries,
-        mainCountryHistory
-      ),
-      round: 1,
-      hasGameEnded: false,
-    }));
-    mainCountryHistory = [];
-  };
-
-  // Going to the next round
-  const nextRound = () => {
-    setProgress(0);
-    setGameState(prevValue => ({
-      ...prevValue,
-      mainCountry: pickRandomCountryWithBorders(
-        noBorderCountries,
-        mainCountryHistory
-      ),
-      round: ++prevValue.round,
-      hasGameEnded: false,
-    }));
-  };
-
-  const handleRightAnswer = () => {
-    setGameState(prevValue => ({
-      ...prevValue,
-      rightAnswers: prevValue.rightAnswers + 1,
-    }));
-  };
+    if (gameState.mainCountry && gameState.countries) {
+      setCountryCards(cardPick(gameState.mainCountry, gameState.countries));
+    }
+  }, [gameState.mainCountry, gameState.countries]);
 
   return (
-    <div>
-      <div className="game-panel">
-        <Sidebar
-          round={gameState.round}
-          score={gameState.score}
-          gameState={gameState.hasGameEnded}
-          reset={gameReset}
-          next={nextRound}
-        />
-        <main>
-          <Title country={gameState.mainCountry} />
-          <ProgressBar progress={progress} />
-          <CardArea>
-            {countryCards.map((card, index) => (
-              <Card
-                key={index}
-                country={card}
-                borders={gameState.mainCountry.borders ?? []}
-                progress={setProgress}
-                rightAnswers={gameState.rightAnswers}
-                setRigthAnswers={handleRightAnswer}
-              />
-            ))}
-          </CardArea>
-        </main>
-      </div>
+    <div className="game-panel">
+      <Sidebar />
+      <main>
+        <Title /> {/*TODO? have props so it renders for memo */}
+        <ProgressBar />
+        <CardArea>
+          {countryCards.map(country => (
+            <CountryCard key={country.cca3} country={country} />
+          ))}
+        </CardArea>
+      </main>
     </div>
   );
 };
 
 export default App;
-
-interface IGameState {
-  round: number;
-  score: number;
-  mainCountry: ICountry | ILoadingCountry;
-  hasGameEnded: boolean;
-  rightAnswers: number;
-}
-
-export interface ILoadingCountry {
-  name: {
-    official: string;
-    common: string;
-  };
-  cca2: string;
-  borders: string[];
-}
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,20 +1,22 @@
+import { useContext } from "react";
 import styles from "./Title.module.scss";
+import GameInfoContext from "../store/game-info-context";
 
 import { getEmojiForCountry } from "../logic";
-import type ICountry from "./types/country.interface";
-import type { ILoadingCountry } from "../App";
 
-const Title = ({ country }: Props) => (
-  <section className={styles["selected-country"]}>
-    <span className={styles["selected-country__flag"]}>
-      {getEmojiForCountry(country.cca2)}
-    </span>
-    <h1 className={styles["selected-country__name"]}>{country.name.common}</h1>
-  </section>
-);
+const Title = () => {
+  const { mainCountry } = useContext(GameInfoContext);
+
+  return (
+    <section className={styles["selected-country"]}>
+      <span className={styles["selected-country__flag"]}>
+        {getEmojiForCountry(mainCountry?.cca2 ?? "")}
+      </span>
+      <h1 className={styles["selected-country__name"]}>
+        {mainCountry?.name.common ?? "Loading..."}
+      </h1>
+    </section>
+  );
+};
 
 export default Title;
-
-interface Props {
-  country: ICountry | ILoadingCountry;
-}
