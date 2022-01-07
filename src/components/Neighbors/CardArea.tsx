2bb9b6a0caf2d9fe000efc3e770fbb7921d6cf85
Stavros <snikoletopoulos@hotmail.com>
2022-01-08 01:04:46 +0200

    Show modal with prop trigger

--- src/components/Neighbors/CardArea.tsx
+++ src/components/Neighbors/CardArea.tsx
@@ -1,11 +1,17 @@
 import styles from "./CardArea.module.scss";
 
+import Modal from "../UI/Modal";
+
 const CardArea = (props: Props) => (
-  <section className={styles['neighbours']}>{props.children}</section>
+  <>
+    { props.showModal && <Modal />}
+    <section className={styles["neighbours"]}>{props.children}</section>
+  </>
 );
 
 export default CardArea;
 
 interface Props {
   children: React.ReactNode;
+  showModal: boolean;
 }
