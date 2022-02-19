8dda0fc74de75edcad9aed081dc55b367264278a
Stavros <snikoletopoulos@hotmail.com>
2022-02-19 16:31:10 +0200

    Wrap App with Redux store

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import ErrorBoundary from "./components/ErrorBoundary";
-import { GameInfoProvider } from "./store/Context/game-info-context";
+import { Provider } from "react-redux";
+import  store  from "./store";
 
 ReactDOM.render(
 	<StrictMode>
 		<ErrorBoundary>
-			<GameInfoProvider>
+			<Provider store={store}>
 				<App />
-			</GameInfoProvider>
+			</Provider>
 		</ErrorBoundary>
 	</StrictMode>,
 	document.getElementById("root")
