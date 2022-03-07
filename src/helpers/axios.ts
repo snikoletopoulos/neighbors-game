fb449c41f9eee9da56c481e8c2b3dd1874baccb8
Stavros <snikoletopoulos@hotmail.com>
2022-03-07 20:00:33 +0200

    Remove console.logs

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -9,11 +9,9 @@ export const getAxiosError = (error: AxiosError) => {
 		// The request was made but no response was received
 		// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
 		// http.ClientRequest in node.js
-		console.log(error.request);
 		return error.request;
 	} else {
 		// Something happened in setting up the request that triggered an Error
-		console.log("Error", error.message);
 		return error.message || error;
 	}
 };
