d462c32a9ccf5090b3f49e1140e89fe1d43bee13
Stavros <snikoletopoulos@hotmail.com>
2022-03-07 22:21:23 +0200

    Initial configuration

---
+++ src/i18n/index.ts
@@ -0,0 +1,11 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+	resources: {},
+	lng: "en",
+	fallbackLng: "en",
+	interpolation: {
+		escapeValue: false,
+	},
+});
