51ffe223d5fb63d0090dfb969fa7ba8f26cd7b60
Stavros <snikoletopoulos@hotmail.com>
2022-03-08 02:12:04 +0200

    Add translations
    Create Greek translation file and add some translations

--- src/i18n/translations/en.ts
+++ src/i18n/translations/en.ts
@@ -1,1 +1,9 @@
-export default {};
+export default {
+	win_text: "You won! Congratulations!",
+	lose_text: "You lost. Try again!",
+	new_game: "New Game",
+	next_round: "Next Country",
+	score: "Score",
+	round: "Round",
+	loading: "Loading",
+};
---
+++ src/i18n/translations/gr.ts
@@ -0,0 +1,8 @@
+export default {
+	win_text: "Kερδισες! Συγχαρητηρια!",
+	lose_text: "Έχασες. Προσπάθησε ξανά!",
+	new_game: "Νέο Παιχνίδι",
+	next_round: "Επόμενη χώρα",
+	score: "Σκορ",
+	round: "Γύρος",
+};
