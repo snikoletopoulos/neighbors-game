346a441b2c41eb614ee7b20ec7a5bed295f77efd
Stavros <snikoletopoulos@hotmail.com>
2022-03-08 02:11:25 +0200

    Import i18n to main file to register it

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import ErrorBoundary from "components/ErrorBoundary";
 import { Provider } from "react-redux";
 import store from "store";
+import "i18n";
 
 ReactDOM.render(
 	<StrictMode>
