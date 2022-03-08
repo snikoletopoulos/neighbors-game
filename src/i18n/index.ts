8f6d0e9afafc12f549521404f47bf10fa18e2d08
Stavros <snikoletopoulos@hotmail.com>
2022-03-08 02:12:17 +0200

    Register translations to config file

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import en from "i18n/translations/en";
+import gr from "i18n/translations/gr";
+
 i18n.use(initReactI18next).init({
-	resources: {},
+	resources: {
+		en: {
+			translation: en,
+		},
+		gr: {
+			translation: gr,
+		},
+	},
 	lng: "en",
 	fallbackLng: "en",
 	interpolation: {
