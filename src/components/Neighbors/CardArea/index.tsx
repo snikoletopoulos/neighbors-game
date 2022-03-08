ec4da982429c501a3bebe3f1ca309a4511037b0e
Stavros <snikoletopoulos@hotmail.com>
2022-03-08 02:28:56 +0200

    Translate modal header

--- src/components/Neighbors/CardArea/index.tsx
+++ src/components/Neighbors/CardArea/index.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import styles from "./CardArea.module.scss";
 
+import { useTranslation } from "react-i18next";
 import Modal from "components/UI/Modal";
 
 interface Props {
@@ -8,11 +9,17 @@ interface Props {
 	showModal: boolean;
 	message: string;
 }
-const CardArea: React.FC<Props> = props => (
-	<section className={styles["neighbours"]}>
-		{props.showModal && <Modal header="Game over" body={props.message} />}
-		{props.children}
-	</section>
-);
+const CardArea: React.FC<Props> = props => {
+	const { t } = useTranslation();
+
+	return (
+		<section className={styles["neighbours"]}>
+			{props.showModal && (
+				<Modal header={t("game_over")} body={props.message} />
+			)}
+			{props.children}
+		</section>
+	);
+};
 
 export default memo(CardArea);
